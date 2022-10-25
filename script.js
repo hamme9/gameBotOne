6356c8e373dca28e055ac6e9b1652c551a74c696
hamme9 <arseniy-2002@bk.ru>
2022-10-25 20:20:21 +0700

    create game-bot  guessing a random number from 1 to 100

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 'use strict';
 
+const randomNumber = getRandomInt(100);
+console.log(randomNumber);
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num) && num[0] != ' ' && num.slice(-1) != ' ' && num >= 0;
+};
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const gameBot = function(x) {
+    function count() {
+        const number = prompt('Угадай число от 1 до 100');
+        switch(true) {
+            case number === null:
+                alert('Игра окончена');
+                break;
+            case number == x:
+                alert('Поздравляю, Вы угадали!!!');
+                break;
+            case !isNumber(number):
+                alert('Введи число!');
+                console.log(number);
+                return count();
+            case number < x:
+                alert('Загаданное число больше');
+                console.log(number);
+                return count();
+            case number > x:
+                alert('Загаданное число меньше');
+                console.log(number);
+                return count();
+        }
+    }
+    count();
+};
+
+gameBot(randomNumber);
