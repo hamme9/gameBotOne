1b85af91aee834f45d5319bc18e8e34c1d9c31dd
hamme9 <arseniy-2002@bk.ru>
2022-10-25 20:52:37 +0700

    removed the global randomNumber variable, removed the x argument from the gameBot function, changed the logic so that only one call to count remains inside count (not three);

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const randomNumber = getRandomInt(100);
-console.log(randomNumber);
-
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num) && num[0] != ' ' && num.slice(-1) != ' ' && num >= 0;
 };
@@ -11,31 +8,34 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const gameBot = function(x) {
+const gameBot = function() {
+    const x = getRandomInt(100);
+    console.log(x);
     function count() {
         const number = prompt('Угадай число от 1 до 100');
         switch(true) {
             case number === null:
                 alert('Игра окончена');
-                break;
+                return null;
             case number == x:
                 alert('Поздравляю, Вы угадали!!!');
-                break;
+                return x;
             case !isNumber(number):
                 alert('Введи число!');
                 console.log(number);
-                return count();
+                break;
             case number < x:
                 alert('Загаданное число больше');
                 console.log(number);
-                return count();
+                break;
             case number > x:
                 alert('Загаданное число меньше');
                 console.log(number);
-                return count();
+                break;
         }
+        count();
     }
     count();
 };
 
-gameBot(randomNumber);
+gameBot();
